ed17eff77fc51c6b3fe3cf6ec8c7a1d2d8ac6e4d
carrottechplus <blohzzang@gmail.com>
2023-07-10 10:23:37 +0900

    부모에 참조형자료가 있으면 자식에서 memo 풀림을 방지하고자 lodash 설치

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ function App() {
 	console.log('parent');
 	const [Counter, setCounter] = useState(0);
 	const [Input, setInput] = useState('');
+	const colors = ['red', 'green', 'blue'];
+
 	return (
 		<div>
 			<h1>Hello</h1>
 			<button onClick={() => setCounter(Counter + 1)}>plus</button>
 			<p>{Counter}</p>
 			<input type='text' value={Input} onChange={(e) => setInput(e.target.value)} />
-			<Child Counter={Counter} />
+			<Child Counter={Counter} color={colors} />
 		</div>
 	);
 }
--- src/Child.js
+++ src/Child.js
@@ -1,4 +1,7 @@
 import { memo } from 'react';
+//props로 전달되는 값이 참조형 자료일 떄 참조링크값을 비교하는 것이 아닌 참조되고 있는 원본 데이터가 같은지를 비교.
+import { isEqual } from 'lodash';
+
 function Child(props) {
 	console.log('child');
 	return (
@@ -8,4 +11,4 @@ function Child(props) {
 	);
 }
 
-export default memo(Child);
+export default memo(Child, isEqual);
