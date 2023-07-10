a7ca1ec914e578c8f2e1348bb8b01df842a8e5ab
carrottechplus <blohzzang@gmail.com>
2023-07-10 10:01:02 +0900

    자식 컴포넌트에 props로 전달하지 않은 값이 수정될땐 memoization 유지

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Child from './Child';
 function App() {
 	console.log('parent');
 	const [Counter, setCounter] = useState(0);
+	const [Input, setInput] = useState('');
 	return (
 		<div>
 			<h1>Hello</h1>
 			<button onClick={() => setCounter(Counter + 1)}>plus</button>
 			<p>{Counter}</p>
+			<input type='text' value={Input} onChange={(e) => setInput(e.target.value)} />
 			<Child Counter={Counter} />
 		</div>
 	);
